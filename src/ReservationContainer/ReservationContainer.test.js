8bc6688b16302ccc4cc26eb6cdfc4b3076b0786c
Zoe <zoekharvey@gmail.com>
2019-10-17 10:32:10 -0600

    Add snapshot test to res cards

--- src/ReservationContainer/ReservationContainer.test.js
+++ src/ReservationContainer/ReservationContainer.test.js
@@ -8,7 +8,6 @@ describe('Reservation Container', () => {
     { name: 'Cat', date: '2019/03/03', time: '7:00', number: 2, id: 10 }]
     const wrapper = shallow(<ReservationContainer
       reservations={reservations}
-      // removeReservation={jest.fn()}
       />);
     expect(wrapper).toMatchSnapshot();
   });
